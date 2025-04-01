6949038a627e950a31d8621acec156864c5c1cc6
Arjunan <arjunan@sketchbrahma.com>
2025-04-01 15:36:33 +0530

    fix the form modal

--- src/containers/Webinar/index.jsx
+++ src/containers/Webinar/index.jsx
@@ -1,11 +1,9 @@
-import React from 'react'
 import Winning from './Winning'
 import Master from './Master'
 import Testimonials from './Testimonials'
 import Services1 from './Services1'
 import SlotBooking from './SlotBooking'
 import Solution from './Solution'
-import ContactUs1 from "../../components/ContactUs1";  
 
 
 
@@ -15,7 +13,6 @@ export default function Webinar() {
     <>
     <Winning/>
     <Solution/>
-    <ContactUs1/>
     <Master/>
     <Testimonials/>
     <Services1/>
