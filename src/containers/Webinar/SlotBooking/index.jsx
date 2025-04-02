e10687a1473086f68ec03897bd64cf054a4915ff
santhosh v <santhoshv@sketchbrahma.com>
2025-04-02 12:58:34 +0530

    position center div fixed

--- src/containers/Webinar/SlotBooking/index.jsx
+++ src/containers/Webinar/SlotBooking/index.jsx
@@ -2,9 +2,9 @@
 
 import "./slotbooking.scss";
 import Reserve from '../../../components/Reserve';
-const SlotBooking= () => {
+const SlotBooking= ({modClass = ""}) => {
   return (
-    <section className="container slotbooking">
+    <section className={`container slotbooking ${modClass}`}>
         <div className="booking">
            <Reserve/>
            <p id="p1">Can&apos;t attend? Register to get the recording. Let&apos;s improve your apps and websites for users!</p>
